7a4777546397fe8b795e415650fc5d3b32db8aba
yifengyou <842056007@qq.com>
2023-02-19 23:05:50 +0800

    添加一键编译脚本go.sh 解析两个工具

--- go.sh
+++ go.sh
@@ -12,4 +12,10 @@ make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- -j`nproc`
 #make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- rk3399-evb.img
 make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- rk3399-firefly-linux.img
 
+# generate resource.img boot.img zboot.img
+./scripts/mkimg --dtb rk3399-firefly-linux.dtb
+
+# generate kernel.img
+./scripts/mkkrnlimg arch/arm64/boot/Image kernel.img
+
 echo "All done!"
