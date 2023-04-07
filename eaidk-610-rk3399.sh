6da3a07b83b1c30b466f85a8972730fda0326de7
yifengyou <842056007@qq.com>
2023-04-07 13:50:41 +0800

    clean repo

--- eaidk-610-rk3399.sh
+++ eaidk-610-rk3399.sh
@@ -2,18 +2,20 @@
 
 set -xe
 
+make distclean
+
 ln -svf /usr/bin/python3 /usr/bin/python
 
-make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- rockchip_linux_defconfig
+make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- eaidk-610-rk3399_defconfig
 
 #make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- 
 make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- -j`nproc`
 
 #make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- rk3399-evb.img
-make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- rk3399-firefly-linux.img
+make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- rk3399-eaidk-610.img
 
 # generate resource.img boot.img zboot.img
-./scripts/mkimg --dtb rk3399-firefly-linux.dtb
+./scripts/mkimg --dtb rk3399-eaidk-610.dtb
 
 # generate kernel.img
 ./scripts/mkkrnlimg arch/arm64/boot/Image kernel.img
